2c5bc2c7163eeb7a59e28240fc6848533037bdf3
matt <syan4@ualberta.ca>
2021-12-21 22:43:29 -0700

    initial

---
+++ example/src/fontawesome.rs
@@ -0,0 +1,23 @@
+use yew::prelude::*;
+
+// go to https://fontawesome.com/icons/
+// download the desired svg
+// Create a new component using <FontawesomeSvg> with the `viewBox` attribute and the `d` attribute
+// inside the <path>
+
+// tips: svg color is changeable via css, just select the svg and set color as usual
+
+#[derive(Properties, PartialEq, Clone)]
+pub struct FontawesomeSvgProps {
+    pub view_box: &'static str,
+    pub d: &'static str,
+}
+
+#[function_component(FontawesomeSvg)]
+pub fn fontawesome_svg(props: &FontawesomeSvgProps) -> Html {
+    html! {
+        <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox={props.view_box}>
+            <path fill="currentColor" d={props.d}/>
+        </svg>
+    }
+}
---
+++ example/src/interop.rs
@@ -0,0 +1,52 @@
+use js_sys::{Object, Reflect};
+use wasm_bindgen::{prelude::*, JsValue};
+use web_sys::{HtmlCanvasElement, HtmlImageElement};
+use yew_interop::declare_resources;
+
+declare_resources!(
+toast
+"https://cdn.jsdelivr.net/npm/toastify-js@1.11.2/src/toastify.min.js"
+"https://cdn.jsdelivr.net/npm/toastify-js@1.11.2/src/toastify.min.css"
+cropper
+"https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js"
+"https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css"
+);
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_name = Toastify)]
+    pub type Toast;
+
+    #[wasm_bindgen(constructor, js_class = "Toastify")]
+    pub fn new(config: &JsValue) -> Toast;
+
+    #[wasm_bindgen(method, structural, js_class = "Toastify", js_name = showToast)]
+    pub fn show_toast(this: &Toast);
+}
+
+pub fn show_congrats_toast() {
+    let config = Object::new();
+    Reflect::set(
+        &config,
+        &"text".into(),
+        &"Yew is awesome!".to_string().into(),
+    )
+    .ok();
+    let toast = Toast::new(&config);
+    toast.show_toast();
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_name = Cropper)]
+    pub type Cropper;
+
+    #[wasm_bindgen(constructor, js_class = "Cropper")]
+    pub fn new(image: &HtmlImageElement, options: &JsValue) -> Cropper;
+
+    #[wasm_bindgen(method, structural, js_class = "Cropper", js_name = destroy)]
+    pub fn destroy(this: &Cropper);
+
+    #[wasm_bindgen(method, structural, js_class = "Cropper", js_name = getCroppedCanvas)]
+    pub fn get_cropped_canvas(this: &Cropper) -> HtmlCanvasElement;
+}
