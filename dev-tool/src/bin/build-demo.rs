8f88d8fe5c019b684c5171894f62fae9e2655c56
matt <syan4@ualberta.ca>
2022-01-02 01:49:25 -0700

    hi ci: i messed up, pls fix

--- dev-tool/src/bin/build-demo.rs
+++ dev-tool/src/bin/build-demo.rs
@@ -38,6 +38,10 @@ struct Args {
     /// master, or a full semver string like 2.1.0
     #[clap(parse(try_from_str))]
     version: Version,
+
+
+
+
 }
 
 fn main() -> ! {
--- example/src/fontawesome.rs
+++ example/src/fontawesome.rs
@@ -11,6 +11,9 @@ use yew::prelude::*;
 pub struct FontawesomeSvgProps {
     pub view_box: &'static str,
     pub d: &'static str,
+
+
+
 }
 
 #[function_component(FontawesomeSvg)]
